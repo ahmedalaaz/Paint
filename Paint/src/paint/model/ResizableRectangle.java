dbaeb704e86de4d200289b84efcefc8c492a08fa
Ahmed <ahmed.alansary08@gmail.com>
2018-04-21 19:45:46 +0200

    Fixed resize rectangle bug

--- Paint/src/paint/model/ResizableRectangle.java
+++ Paint/src/paint/model/ResizableRectangle.java
@@ -251,7 +251,7 @@ public class ResizableRectangle {
 	        });
 	        outerSelectorRectangle.setStrokeWidth(6d);
 	        outerSelectorRectangle.getStrokeDashArray().addAll(46d,2d,4d);
-	        outerSelectorRectangle.setFill(Color.WHITE);
+	        outerSelectorRectangle.setFill(Color.TRANSPARENT);
 		}
 
     private void setUpDragging(Circle circle, Wrapper<Point2D> mouseLocation) {
